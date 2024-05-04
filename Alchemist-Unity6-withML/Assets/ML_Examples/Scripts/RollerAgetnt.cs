2c3f70c35383d9f13ebd21d249106840e8a386f9
Yi-Chun Chen <ychen74@ncsu.edu>
2024-05-04 03:01:04 -0400

    update isometric scene test

---
+++ Alchemist-Unity6-withML/Assets/ML_Examples/Scripts/RollerAgetnt.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class RollerAgetnt : Agent
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+}
